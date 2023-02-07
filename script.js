d13e3beeaf25a7134c0a7ba8dec428f0477c4746
shushtain <shushtain@gmail.com>
2023-02-07 16:43:30 +0200

    Beige accent

--- script.js
+++
@@ -1,5 +0,0 @@
-let rnd = Math.floor(Math.random() * (200 - 30) + 30);
-
-window.addEventListener('load', function() {
-  document.querySelector(':root').style.setProperty('--color-accent', 'hsl(' + rnd + ',100%,50%)');
-});
